7b977a866e8b3e304b1f7bb75a86bdc00871b7a0
Dhelprat <65176876+Dhelprat@users.noreply.github.com>
2024-12-20 19:18:50 +0300

    fixed

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -6,6 +6,13 @@ int main() {
     std::cout << "Введите число: ";
     std::cin >> number;
 
+    for (char ch : number) {
+        if (!isdigit(ch)) {
+            std::cerr << "Введите число!" << std::endl;
+            return 1;
+        }
+    }
+
     if (isCleanNumber(number)) {
         std::cout << "Число является чистым." << std::endl;
     } else {
